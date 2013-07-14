d4b18cfbff39052ec3a447ff1e14924aa771bedf
DasRegal <plexus.9310@gmail.com>
2013-07-14 23:14:54 +0700

    [*] pwm_ctrl: файл main.cpp разбит на подпрограммы.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "msp_pwm.h"
 #include "cd74ac153.h"
 #include "sonar.h"
+#include <string.h>
 
 #include <curses.h>
 #include <iostream>
@@ -16,14 +17,6 @@
 
 	CSonar sonar;
 
-int ReadSonar(ESwLine line)
-{
-	cd74ac.SwitchLine(line);
-	return sonar.ReadValue();
-}
-
-
-
 // 30 вывод
 #define MISO 114
 // 34 вывод
@@ -33,11 +26,11 @@ int ReadSonar(ESwLine line)
 
 int buf[5];
 
-	#define		ROLL_SET(x) 	(buf[0] = x * 10 + 1000 + 57)
-	#define		PITCH_SET(x) 	(buf[1] = x * 10 + 1000 + 57)
-	#define 	THROTTLE_SET(x)	(buf[2] = x * 10 + 1000 + 57)
-	#define 	YAW_SET(x)		(buf[3] = x * 10 + 1000 + 57)
-	#define 	MODE_SET(x) 	(buf[4] = x * 10 + 1000 + 57)
+	#define		ROLL_SET(x) 	(buf[0] = x * 10 + 1000 + 230)
+	#define		PITCH_SET(x) 	(buf[1] = x * 10 + 1000 + 230)
+	#define 	THROTTLE_SET(x)	(buf[2] = x * 10 + 1000 + 230)
+	#define 	YAW_SET(x)		(buf[3] = x * 10 + 1000 + 230)
+	#define 	MODE_SET(x) 	(buf[4] = x * 10 + 1000 + 230)
 
 int* g_Prc;
 int g_Prc1;
@@ -45,6 +38,12 @@ int g_Prc2;
 int g_Prc3;
 int g_Prc4;
 int g_Prc5;
+
+int ReadSonar(ESwLine line)
+{
+	cd74ac.SwitchLine(line);
+	return sonar.ReadValue();
+}
 
 void Delay(int time)
 {
@@ -58,10 +57,8 @@ void Init(void)
 	g_Prc3 = 0;
 	g_Prc4 = 50;
 	g_Prc5 = 50;
-	// sonar.Create("ttySAC3");
-	// cd74ac.Init();	
-	// cd74ac.SwitchLine(SONAR2);
-
+	sonar.Create("ttySAC3");
+	cd74ac.Init();	
 }
 
 void KeyUP(int* prc)
@@ -79,7 +76,7 @@ void KeyDOWN(int* prc)
 		*prc = 0;
 }
 
-int main()
+int main(int argc, char const *argv[])
 {
 	int x;
 	InitSPI2PWM(MISO, MOSI, CLK);
@@ -87,132 +84,197 @@ int main()
 
 	Init();
 
-	ROLL_SET(50);
-	PITCH_SET(50);
-	THROTTLE_SET(0);
-	YAW_SET(110);
-	MODE_SET(0);
-	SetSPI2PWM(buf, 5);
-
-	Delay(6);
-	YAW_SET(50);
-	SetSPI2PWM(buf, 5);
-	printf("Start\n");
-
-	// THROTTLE_SET(60);
-	// SetSPI2PWM(buf, 5);
-	// Delay(2);
-
-	// MODE_SET(100);
-	// SetSPI2PWM(buf, 5);
-
-	// PITCH_SET(40);
-	// SetSPI2PWM(buf, 5);
-
-	// int data;
-	// while(1)
-	// {
-	// 	data = sonar.ReadValue();
-	// 	printf("%d\n", data);
-	// 	if (data > 2500 && data < 4000)
-	// 	{	
-	// 		YAW_SET(60);
-	// 		SetSPI2PWM(buf, 5);
-	// 	}
-	// 	else
-	// 	if (data < 1500)
-	// 	{
-	// 		YAW_SET(40);
-	// 		SetSPI2PWM(buf, 5);
-	// 	}
-	// 	else break;
-	// }
-
-
-
-	// PITCH_SET(50);
-	// SetSPI2PWM(buf, 5);
-
-	// THROTTLE_SET(40);
-	// SetSPI2PWM(buf, 5);
-	// Delay(3);
-	// THROTTLE_SET(20);
-	// SetSPI2PWM(buf, 5);
-	// Delay(3);
-	// THROTTLE_SET(0);
-	// SetSPI2PWM(buf, 5);
-
-	// YAW_SET(0);
-	// SetSPI2PWM(buf, 5);
-	// Delay(6);
-	// YAW_SET(50);
-	// SetSPI2PWM(buf, 5);
-
-	// int t = 5;
-	// int dt;
-	// int dprc = 0;
-	
-	// dt = (t * 1000 * 1000) / 100;
-	
-	// // for (int i = 0; i < (t * 1000 * 1000); i += dt)
-	// // {
-	// // 	YAW_SET(dprc);
-	// // 	SetSPI2PWM(buf, 5);
-	// // 	dprc++;
-	// // 	usleep(dt);
-	// // }
-	
-
-	initscr();	
-	raw();
-	keypad(stdscr, TRUE);
-	noecho();
-	
-	printw("GO!");
-
-	bool loop;
-	loop = true;
-	char ch;
-	int chan;
-	chan = 1;
-	g_Prc = &g_Prc1;
-	while( loop == true)
+	if (argc == 1)
 	{
-		ch = getch();
-		switch (ch)
-		{
-			case 'w': KeyUP(g_Prc); printw("%d ", *g_Prc); break;
-			case 's': KeyDOWN(g_Prc); printw("%d ", *g_Prc); break;
-			case 'a': MODE_SET(0);   break;
-			case 'd': MODE_SET(100); break;
-			case '1': printw("Roll: "); chan = 1; break;
-			case '2': printw("Pitch: "); chan = 2; break;
-			case '3': printw("Throttle: "); chan = 3; break;
-			case '4': printw("Yaw: "); chan = 4; break;
-			case '5': printw("Mode: "); chan = 5; break;
-			case 'q':
-			case 'Q':loop = false;
-		}
-		
-		switch (chan)
-		{
-			case 1: g_Prc = &g_Prc1; ROLL_SET(*g_Prc); break;
-			case 2: g_Prc = &g_Prc2; PITCH_SET(*g_Prc); break;
-			case 3: g_Prc = &g_Prc3; THROTTLE_SET(*g_Prc); break;
-			case 4: g_Prc = &g_Prc4; YAW_SET(*g_Prc); break;
-			case 5: g_Prc = &g_Prc5; MODE_SET(*g_Prc); break;
-		}
+		printf("No params\n");
+		return 1;
+	}
+
+	//==========================================
+	//				    ARMED
+	//==========================================
+	if ((argc > 1) && strcmp(argv[1], "nostart") != 0)
+	{	
+		printf("Armed\n");
+		Delay(5);
+		ROLL_SET(35);
+		PITCH_SET(34);
+		THROTTLE_SET(0);
+		YAW_SET(40);
+		MODE_SET(0);
+		SetSPI2PWM(buf, 5);
+	
+		Delay(1);
+		YAW_SET(100);
+		SetSPI2PWM(buf, 5);
+	
+		Delay(4);
+		YAW_SET(40);
+		SetSPI2PWM(buf, 5);
+		printf("Start\n");
+	}
+
+	//==========================================
+	// 					ВЗЛЕТ
+	//==========================================
+	if ((argc > 1) && (strcmp(argv[1],"kt3") == 0 || strcmp(argv[1], "sonar") == 0))
+	{
+		printf("Взлет\n");
+		THROTTLE_SET(75);
+		SetSPI2PWM(buf, 5);
+		Delay(2);
+
+		MODE_SET(100);
+		SetSPI2PWM(buf, 5);
+
+		// Вперед лететь
+		PITCH_SET(23);
 		SetSPI2PWM(buf, 5);
 	}
-	refresh();
-	getch();
-	endwin();
 
-	// YAW_SET(0);
-	// SetSPI2PWM(buf, 5);
-	// Delay(5);
-	// YAW_SET(50);
-	// SetSPI2PWM(buf, 5);
+	if ((argc > 1) && !strcmp(argv[1], "test"))
+	{
+		int data;
+		while(1)
+		{
+			data = ReadSonar(SONAR3);
+			printf("Data: - %d\n", data);
+		}
+	}
+	//==========================================
+	// 			ПОЛЕТ ПО СОНАРАМ
+	//==========================================
+	if ((argc > 1) && !strcmp(argv[1], "sonar"))
+	{	
+		printf("Полет по сонарам\n");
+		int data;
+		while(1)
+		{
+			data = ReadSonar(SONAR3);
+			printf("Data: - %d\n", data);
+			if (data > 2500 && data < 4000)
+			{	
+				YAW_SET(60);
+				SetSPI2PWM(buf, 5);
+			}
+			else
+			if (data < 1500)
+			{
+				YAW_SET(40);
+				SetSPI2PWM(buf, 5);
+			}
+			else break;
+		}
+	}
+
+	//==========================================
+	// 				ПОЛЕТ ВПЕРЕД
+	//==========================================
+	if ((argc > 1) && !strcmp(argv[1], "kt3"))
+	{
+		printf("Полет вперед\n");
+		Delay(8);
+	}
+
+	//==========================================
+	// 					ПОСАДКА
+	//==========================================
+	if ((argc > 1) && (!strcmp(argv[1], "kt3") || !strcmp(argv[1], "sonar")))
+	{
+		PITCH_SET(37);
+		SetSPI2PWM(buf, 5);
+		Delay(1);
+
+		printf("Посадка\n");
+		PITCH_SET(34);
+		MODE_SET(0);
+		SetSPI2PWM(buf, 5);
+	
+		THROTTLE_SET(62);
+		SetSPI2PWM(buf, 5);
+		Delay(2);
+		
+		THROTTLE_SET(0);
+		SetSPI2PWM(buf, 5);
+	}
+	
+	//==========================================
+	//		   		РУЧНОЙ РЕЖИМ
+	//==========================================
+	if (argc > 1 && strcmp(argv[1], "arm") == 0)
+	{
+		printf("Ручной режим\n");
+		initscr();	
+		raw();
+		keypad(stdscr, TRUE);
+		noecho();
+		
+		printw("GO!");
+	
+		bool loop;
+		loop = true;
+		char ch;
+		int chan;
+		chan = 1;
+		g_Prc = &g_Prc1;
+		while( loop == true)
+		{
+			ch = getch();
+			switch (ch)
+			{
+				case 'w': KeyUP(g_Prc); printw("%d ", *g_Prc); break;
+				case 's': KeyDOWN(g_Prc); printw("%d ", *g_Prc); break;
+				case 'a': printw("Mode stabilize. "); MODE_SET(0);   break;
+				case 'd': printw("Mode Althold. "); MODE_SET(100); break;
+				case 'z': printw("ThrUP. "); THROTTLE_SET(100); SetSPI2PWM(buf, 5); break;
+				case 'c': printw("ThrDOWN. "); THROTTLE_SET(0); SetSPI2PWM(buf, 5); break;
+				case '1': printw("Roll: "); chan = 1; break;
+				case '2': printw("Pitch: "); chan = 2; break;
+				case '3': printw("Throttle: "); chan = 3; break;
+				case '4': printw("Yaw: "); chan = 4; break;
+				case '5': printw("Mode: "); chan = 5; break;
+				case '[': printw("Start... "); THROTTLE_SET(0); YAW_SET(100); SetSPI2PWM(buf, 5); Delay(4); YAW_SET(50); SetSPI2PWM(buf, 5); printw("Ok. "); break;
+				case ']': printw("Stop... "); THROTTLE_SET(0); YAW_SET(0); SetSPI2PWM(buf, 5); Delay(4); YAW_SET(50); SetSPI2PWM(buf, 5); printw("Ok. "); break;
+				case 'r': {
+						printw("Reset... ");
+						ROLL_SET(50);
+						PITCH_SET(50);
+						THROTTLE_SET(0);
+						YAW_SET(50);
+						MODE_SET(0);
+						SetSPI2PWM(buf, 5);
+						printw("Ok. ");
+						break;
+				}
+				case 'q':
+				case 'Q':loop = false;
+			}
+			
+			switch (chan)
+			{
+				case 1: g_Prc = &g_Prc1; ROLL_SET(*g_Prc); break;
+				case 2: g_Prc = &g_Prc2; PITCH_SET(*g_Prc); break;
+				case 3: g_Prc = &g_Prc3; THROTTLE_SET(*g_Prc); break;
+				case 4: g_Prc = &g_Prc4; YAW_SET(*g_Prc); break;
+				case 5: g_Prc = &g_Prc5; MODE_SET(*g_Prc); break;
+			}
+			SetSPI2PWM(buf, 5);
+		}
+		refresh();
+		getch();
+		endwin();
+	}
+
+	//==========================================
+	// 				  DISARMED
+	//==========================================
+	printf("Disarmed\n");
+	THROTTLE_SET(0);
+	YAW_SET(0);
+	SetSPI2PWM(buf, 5);
+	Delay(4);
+	YAW_SET(40);
+	SetSPI2PWM(buf, 5);
 
 	return 0;
 }
